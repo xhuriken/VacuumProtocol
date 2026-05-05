a88d46fe627fc37462780827663165a546bc19b9
xhuriken <73635763+xhuriken@users.noreply.github.com>
2026-05-05 18:42:38 +0200

    Done !

--- Assets/1_Scripts/Network/MyNetworkManager.cs
+++ Assets/1_Scripts/Network/MyNetworkManager.cs
@@ -11,28 +11,11 @@ public class MyNetworkManager : NetworkManager
     [SerializeField] private GameObject _gamePlayerPrefab;
     public List<PlayerObjectController> GamePlayers { get; } = new List<PlayerObjectController>();
 
-    public override void OnServerSceneChanged(string sceneName)
-    {
-        if (sceneName.StartsWith("SteamTest")) // Using StartsWith in case of variations
-        {
-            // We need a temporary list because ReplacePlayerForConnection might affect the original list
-            List<PlayerObjectController> playersToReplace = new List<PlayerObjectController>(GamePlayers);
-
-            foreach (var lobbyPlayer in playersToReplace)
-            {
-                GameObject gamePlayerInstance = Instantiate(_gamePlayerPrefab);
-                
-                // Mirror logic: Replace the Lobby Player object with the Game Player object
-                // for this specific connection. The old object is destroyed automatically.
-                NetworkServer.ReplacePlayerForConnection(lobbyPlayer.connectionToClient, gamePlayerInstance);
-            }
-        }
-    }
-
     public override void OnServerAddPlayer(NetworkConnectionToClient conn)
     {
-        //base.OnServerAddPlayer(conn);
-        if (SceneManager.GetActiveScene().name == "Lobby")
+        string sceneName = SceneManager.GetActiveScene().name;
+
+        if (sceneName == "Lobby")
         {
             PlayerObjectController _playerInstance = Instantiate(_playerPrefab);
             _playerInstance.ConnectionId = conn.connectionId;
@@ -40,6 +23,16 @@ public class MyNetworkManager : NetworkManager
             _playerInstance.PlayerSteamId = (ulong)SteamMatchmaking.GetLobbyMemberByIndex((CSteamID)SteamLobby.Instance.CurrentLobbyId, GamePlayers.Count);
 
             NetworkServer.AddPlayerForConnection(conn, _playerInstance.gameObject);
+            Debug.Log($"[MyNetwork] Lobby Player Added for connection {conn.connectionId}");
+        }
+        else if (sceneName.StartsWith("SteamTest"))
+        {
+            // Spawn the actual game player (Mecha)
+            GameObject gamePlayerInstance = Instantiate(_gamePlayerPrefab);
+            
+            // This replaces the old player object with the new mecha for this connection
+            NetworkServer.AddPlayerForConnection(conn, gamePlayerInstance);
+            Debug.Log($"[MyNetwork] Game Player (Mecha) Added in {sceneName} for connection {conn.connectionId}");
         }
     }
     public override void OnStartServer()
